7d2080ea2c1ff65c0e008ac48834b95d4a9fddae
dan-du-car <dan.du@leidos.com>
2020-05-04 09:06:58 -0700

    available

--- carma-messenger-ui/website/scripts/control.js
+++ carma-messenger-ui/website/scripts/control.js
@@ -157,7 +157,7 @@ function createDivFramePanelText(divFrameId,divFrameCss,divPanelId,divPanelCss,s
             var img = document.createElement('img');
             //image path
             img.src=divTextValue; 
-            img.alt="Image is not availabe";
+            img.alt="Image is not available";
             img.id=divTextId+"Img";
             divText.appendChild(img);
         }else{                    
