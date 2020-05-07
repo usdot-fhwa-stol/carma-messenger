72e0451f2bfbdf2926976f14fbb27695e6790478
qswawrq <qswawrq@gmail.com>
2020-05-07 07:56:11 -0700

    add comments

--- carma-messenger-core/cpp_message/src/cpp_message.cpp
+++ carma-messenger-core/cpp_message/src/cpp_message.cpp
@@ -27,6 +27,7 @@ namespace cpp_message
     {
         nh_.reset(new ros::CARMANodeHandle());
         pnh_.reset(new ros::CARMANodeHandle("~"));
+        // initialize pub/sub
         outbound_binary_message_pub_ = nh_->advertise<cav_msgs::ByteArray>("outbound_binary_msg", 5);
         inbound_binary_message_sub_ = nh_->subscribe("inbound_binary_msg", 5, &Message::inbound_binary_callback, this);
         outbound_geofence_request_message_sub_ = nh_->subscribe("outbound_control_request", 5, &Message::outbound_control_request_callback, this);
@@ -35,19 +36,25 @@ namespace cpp_message
 
     void Message::inbound_binary_callback(const cav_msgs::ByteArrayConstPtr& msg)
     {
+        // only handle ControlRequest for now
         if(msg->messageType == "ControlRequest") {
             j2735_msgs::ControlRequest output;
+            // decode results
             asn_dec_rval_t rval;
 	        MessageFrame_t* message = 0;
+            // copy from vector to array
 	        std::vector<uint8_t> array = msg->content;
             auto len = array.size();
             uint8_t buf[len];
             for(auto i = 0; i < len; i++) {
 	            buf[i] = array[i];
 	        }
+            // use asn1c lib to decode
 	        rval = uper_decode(0, &asn_DEF_MessageFrame, (void **) &message, buf, len, 0, 0);
+
+            // if decode successed
             if(rval.code == RC_OK) {
-                
+                // convert version from char array to string
                 std::string version;
                 auto str_len = message->value.choice.TestMessage04.body.version.size;
                 for(auto i = 0; i < str_len; i++)
@@ -55,16 +62,21 @@ namespace cpp_message
                     version += message->value.choice.TestMessage04.body.version.buf[i];
                 }
                 output.version = version;
+                // copy scale field
                 output.scale = message->value.choice.TestMessage04.body.scale;
+                // copy bounds
                 auto bounds_count = message->value.choice.TestMessage04.body.bounds.list.size;
                 for(auto i = 0; i < bounds_count; i++) {
                     j2735_msgs::ControlBounds bounds;
+                    // copy lat/lon
                     bounds.latitude = message->value.choice.TestMessage04.body.bounds.list.array[i]->lat;
                     bounds.longitude = message->value.choice.TestMessage04.body.bounds.list.array[i]->lon;
+                    // copy offset array to boost vector
                     auto count = message->value.choice.TestMessage04.body.bounds.list.array[i]->offsets.list.size;
                     for(auto j = 0; j < count; j++) {
                         bounds.offsets[j] = *message->value.choice.TestMessage04.body.bounds.list.array[i]->offsets.list.array[j];
                     }
+                    // recover a long value from 8-bit array
                     long temp = 0;
                     for(auto j = 0; j < 8; j++) {
                         temp |= message->value.choice.TestMessage04.body.bounds.list.array[i]->oldest.buf[j];
@@ -73,34 +85,35 @@ namespace cpp_message
                     bounds.oldest = temp;
                     output.bounds.push_back(bounds);
                 }
-                
+                // publish results
                 inbound_geofence_request_message_pub_.publish(output);
             } else {
+                // log warning if failed
                 ROS_WARN_STREAM("Cannot decode ControlRequest message");
             }
-            
+
         }
     }
 
-    // Encode Control Request
     void Message::outbound_control_request_callback(const j2735_msgs::ControlRequestConstPtr& msg)
     {
+        // encode result placeholder
         uint8_t buffer[512];
 	    size_t buffer_size = sizeof(buffer);
 	    asn_enc_rval_t ec;
 	    MessageFrame_t* message;
 	    message = (MessageFrame_t*)calloc(1, sizeof(MessageFrame_t));
+        // if mem allocation fails
 	    if (!message)
         {
 		    ROS_WARN_STREAM("Cannot allocate mem for ControlRequest message encoding");
             return;
 	    }
 
-	    //set default value of testmessage04
+	    //set message type to TestMessage04
 	    message->messageId = 244;
-        message->value.present = MessageFrame__value_PR_TestMessage04;
-        
-        //set version string
+        message->value.present = MessageFrame__value_PR_TestMessage04;        
+        //convert version string to char array
         auto string_size = msg->version.size();
         uint8_t strategy_string_content[string_size];
         for(auto i = 0; i < string_size; i++)
@@ -109,16 +122,19 @@ namespace cpp_message
         }
         message->value.choice.TestMessage04.body.version.buf = strategy_string_content;
         message->value.choice.TestMessage04.body.version.size = string_size;
+        // copy scale
         message->value.choice.TestMessage04.body.scale = msg->scale;
-
+        // copy bounds
         auto count = msg->bounds.size();
         if(count > 0)
         {
             for(auto i = 0; i < count; i++) {
+                // construct control bounds
                 ControlBounds_t* bounds_p;
                 bounds_p = (ControlBounds_t*) calloc(1, sizeof(ControlBounds_t));
                 bounds_p->lat = msg->bounds[i].latitude;
                 bounds_p->lon = msg->bounds[i].longitude;
+                // copy offsets from array to C list struct
                 auto offset_count = msg->bounds[i].offsets.size();
                 if(offset_count > 0) {
                     for(auto j = 0; j < offset_count; j++) {
@@ -126,6 +142,7 @@ namespace cpp_message
                         asn_sequence_add(&bounds_p->offsets.list, &temp);
                     }
                 }
+                // convert a long value to an 8-bit array of length 8
                 uint8_t oldest_val[8];
                 for(auto k = 7; k >= 0; k--) {
                     // TODO this line needs to be tested
@@ -137,19 +154,20 @@ namespace cpp_message
             }
         }
 
-	    //encode message
+	    // encode message
 	    ec = uper_encode_to_buffer(&asn_DEF_MessageFrame, 0, message, buffer, buffer_size);
+        // log a warning if fails
         if(ec.encoded == -1) {
             ROS_WARN_STREAM("Cannot encode ControlRequest message");
             return;
         }
-
-        //copy to byte array msg
+        // copy to byte array msg
         cav_msgs::ByteArray output;
         auto array_length = ec.encoded / 8;
         std::vector<uint8_t> b_array(array_length);
         for(auto i = 0; i < array_length; i++) b_array[i] = buffer[i];
         output.content = b_array;
+        // publish result
         outbound_binary_message_pub_.publish(output);
     }
 
