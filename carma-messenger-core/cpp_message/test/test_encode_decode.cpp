63bfcb9b5ccbf162b306571ce3e504d3b93dd91b
saina-ramyar <saina.ramyar@leidos.com>
2020-05-15 10:17:25 -0400

    new update

--- carma-messenger-core/cpp_message/test/test_encode_decode.cpp
+++ carma-messenger-core/cpp_message/test/test_encode_decode.cpp
@@ -69,68 +69,68 @@ TEST(CppMessageTest, testEncodeControlMsg)
     j2735_msgs::ControlMessage control;
     control.version = "012345678901234567890123456789012345";
     
-    // control.id = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
+    control.id = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
     
-    // control.updated = 0;
+    control.updated = 0;
     
-    // j2735_msgs::VType v_type;
-    // v_type.vehicle_type = 0;
-    // control.vtypes.push_back(v_type);
+    j2735_msgs::VType v_type;
+    v_type.vehicle_type = 0;
+    control.vtypes.push_back(v_type);
 
-    // j2735_msgs::Schedule schedule;
-    // schedule.start = 0;
-    // schedule.end = 0;
+    j2735_msgs::Schedule schedule;
+    schedule.start = 0;
+    schedule.end = 0;
     
-    // schedule.dow_exists = false;
-    // schedule.dow = {false, true, false, false, false, false, false};
+    schedule.dow_exists = false;
+    schedule.dow = {false, true, false, false, false, false, false};
 
-    // schedule.between_exists = false;
-    // schedule.between.start = 0;
-    // schedule.between.end = 0;
-    // schedule.between.utcoffset = 0;
+    schedule.between_exists = false;
+    schedule.between.start = 0;
+    schedule.between.end = 0;
+    schedule.between.utcoffset = 0;
     
 
-    // schedule.repeat_exists = false;
-    // schedule.repeat.duration = 0;
-    // schedule.repeat.interval = 0;
+    schedule.repeat_exists = false;
+    schedule.repeat.duration = 0;
+    schedule.repeat.interval = 0;
 
-    // control.schedule = schedule;
+    control.schedule = schedule;
 
-    // control.regulatory = true;
+    control.regulatory = true;
 
-    // control.control_type.control_type = 0;
+    control.control_type.control_type = 0;
 
-    // control.control_value_exists = false;
-    // control.control_value.value = 0;
+    control.control_value_exists = false;
+    control.control_value.value = 0;
 
-    // control.path_parts = 0;
+    control.path_parts = 0;
 
-    // control.proj = "a";
+    control.proj = "a";
 
-    // control.datum = "a";
+    control.datum = "a";
 
-    // control.time = 0;
+    control.time = 0;
 
-    // control.longitude = 0;
-    // control.latitude = 0;
-    // control.altitude = 0;
-    // control.heading = 0;
+    control.longitude = 0;
+    control.latitude = 0;
+    control.altitude = 0;
+    control.heading = 0;
 
-    // j2735_msgs::Point point1;
-    // point1.x = 0;
-    // point1.y = 0;
-    // point1.z_exists = false;
-    // point1.z = 0;
-    // point1.width = 0;
-    // control.points.push_back(point1);
+    j2735_msgs::Point point1;
+    point1.x = 0;
+    point1.y = 0;
+    point1.z_exists = false;
+    point1.z = 0;
+    point1.width = 0;
+    control.points.push_back(point1);
 
-    // j2735_msgs::Point point2;
-    // point2.x = 0;
-    // point2.y = 0;
-    // point2.z_exists = false;
-    // point2.z = 0;
-    // point2.width = 0;
-    // control.points.push_back(point2);
+    j2735_msgs::Point point2;
+    point2.x = 0;
+    point2.y = 0;
+    point2.z_exists = false;
+    point2.z = 0;
+    point2.width = 0;
+    control.points.push_back(point2);
 
     auto res = worker.encode_geofence_control(control);
     if(res) EXPECT_TRUE(true);
