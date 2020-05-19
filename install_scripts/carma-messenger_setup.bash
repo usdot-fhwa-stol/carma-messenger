442199714aeae5e7ef53a4e3adc469579a2a09ff
dan-du-car <dan.du@leidos.com>
2020-05-19 08:23:23 -0700

    add installation scripts

---
+++ install_scripts/carma-messenger_setup.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#  Copyright (C) 2018-2020 LEIDOS.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#  use this file except in compliance with the License. You may obtain a copy of
+#  the License at
+# 
+#  http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations under
+#  the License.
+
+# Script sets up the /opt/carma-messenger folder for the user
+# Takes in one argument which is the path to the vehicle installation folder to use
+
+if [ ! -d "$1" ]; then
+    echo "Please specify a path to the location of your vehicle installation folder. Such as carma-messenger-config/example_installation_folder/vehicle/"
+    exit -1
+fi
+
+GRP_ID=1000
+sudo groupadd --gid $GRP_ID carma # create the carma messenger group if it does not already exist and add current user to it
+USERNAME=$(whoami)
+sudo usermod -a -G $GRP_ID $USERNAME
+
+sudo mkdir -p /opt/carma-messenger/Desktop /opt/carma-messenger/Pictures 
+sudo chgrp -R $GRP_ID /opt/carma-messenger/
+sudo chmod 775 -R /opt/carma-messenger/
+sudo chmod 775 /opt/carma-messenger/Desktop /opt/carma-messenger/Pictures
+
+
+curl -o /opt/carma-messenger/Desktop/CARMA Messenger.desktop -L https://raw.githubusercontent.com/usdot-fhwa-stol/carma-messenger/develop/install_scripts/Desktop/CARMA Messenger.desktop
+chmod 775 /opt/carma-messenger/Desktop/CARMA Messenger.desktop
+
+curl -o /opt/carma-messenger/Pictures/CARMA_icon_color.png -L https://raw.githubusercontent.com/usdot-fhwa-stol/carma-messenger/develop/install_scripts/Pictures/CARMA_icon_color.png
+chmod 775 /opt/carma-messenger/Pictures/CARMA_icon_color.png
+
+ln -s "$1" /opt/carma/vehicle
