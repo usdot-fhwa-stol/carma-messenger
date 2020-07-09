fb3885e60d513d871c11d81475a5aa8c1b445cb3
afrye-leidos <alexander.frye@leidos.com>
2020-07-09 14:06:59 -0700

    Updated j2735_convertor and control_message_test to use new message types

--- carma-messenger-core/j2735_convertor/include/j2735_convertor/control_message_convertor.h
+++ carma-messenger-core/j2735_convertor/include/j2735_convertor/control_message_convertor.h
@@ -18,6 +18,10 @@
 #include <cstdint>
 #include <j2735_msgs/ControlMessage.h>
 #include <cav_msgs/ControlMessage.h>
+#include <j2735_msgs/TrafficControlMessage.h>
+#include <cav_msgs/TrafficControlMessage.h>
+#include <cav_msgs/OffsetPoint.h>
+
 #include "units.h"
 #include "value_convertor.h"
 
@@ -85,6 +89,116 @@ void convert(const j2735_msgs::DaySchedule& in_msg, cav_msgs::DaySchedule& out_m
  */
 void convert(const j2735_msgs::Point& in_msg, cav_msgs::Point& out_msg);
 
+/**
+ * @brief Convert the contents of a j2735_msgs::DailySchedule into a cav_msgs::DailySchedule
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const j2735_msgs::DailySchedule& in_msg, cav_msgs::DailySchedule& out_msg);
+
+/**
+ * @brief Convert the contents of a j2735_msgs::OffsetPoint into a cav_msgs::OffsetPoint
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const j2735_msgs::OffsetPoint& in_msg, cav_msgs::OffsetPoint& out_msg);
+
+/**
+ * @brief Convert the contents of a j2735_msgs::PathNode into a cav_msgs::PathNode
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const j2735_msgs::PathNode& in_msg, cav_msgs::PathNode& out_msg);
+
+/**
+ * @brief Convert the contents of a j2735_msgs::RepeatParams into a cav_msgs::RepeatParams
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const j2735_msgs::RepeatParams& in_msg, cav_msgs::RepeatParams& out_msg);
+
+/**
+ * @brief Convert the contents of a j2735_msgs::TrafficControlBounds into a cav_msgs::TrafficControlBounds
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const j2735_msgs::TrafficControlBounds& in_msg, cav_msgs::TrafficControlBounds& out_msg);
+
+/**
+ * @brief Convert the contents of a j2735_msgs::TrafficControlDetail into a cav_msgs::TrafficControlDetail
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const j2735_msgs::TrafficControlDetail& in_msg, cav_msgs::TrafficControlDetail& out_msg);
+
+/**
+ * @brief Convert the contents of a j2735_msgs::TrafficControlGeometry into a cav_msgs::TrafficControlGeometry
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const j2735_msgs::TrafficControlGeometry& in_msg, cav_msgs::TrafficControlGeometry& out_msg);
+
+/**
+ * @brief Convert the contents of a j2735_msgs::TrafficControlMessage into a cav_msgs::TrafficControlMessage
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const j2735_msgs::TrafficControlMessage& in_msg, cav_msgs::TrafficControlMessage& out_msg);
+
+/**
+ * @brief Convert the contents of a j2735_msgs::TrafficControlMessageV01 into a cav_msgs::TrafficControlMessageV01
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const j2735_msgs::TrafficControlMessageV01& in_msg, cav_msgs::TrafficControlMessageV01& out_msg);
+
+/**
+ * @brief Convert the contents of a j2735_msgs::TrafficControlParams into a cav_msgs::TrafficControlParams
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const j2735_msgs::TrafficControlParams& in_msg, cav_msgs::TrafficControlParams& out_msg);
+
+/**
+ * @brief Convert the contents of a j2735_msgs::TrafficControlSchedule into a cav_msgs::TrafficControlSchedule
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const j2735_msgs::TrafficControlSchedule& in_msg, cav_msgs::TrafficControlSchedule& out_msg);
+
 ////
 // Convert cav_msgs to j2735_msgs
 ////
@@ -138,5 +252,115 @@ void convert(const cav_msgs::DaySchedule& in_msg, j2735_msgs::DaySchedule& out_m
  * Unit conversions and presence flags are handled
  */
 void convert(const cav_msgs::Point& in_msg, j2735_msgs::Point& out_msg);
+
+/**
+ * @brief Convert the contents of a cav_msgs::DailySchedule into a j2735_msgs::DailySchedule
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const cav_msgs::DailySchedule& in_msg, j2735_msgs::DailySchedule& out_msg);
+
+/**
+ * @brief Convert the contents of a cav_msgs::OffsetPoint into a j2735_msgs::OffsetPoint
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const cav_msgs::OffsetPoint& in_msg, j2735_msgs::OffsetPoint& out_msg);
+
+/**
+ * @brief Convert the contents of a cav_msgs::PathNode into a j2735_msgs::PathNode
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const cav_msgs::PathNode& in_msg, j2735_msgs::PathNode& out_msg);
+
+/**
+ * @brief Convert the contents of a cav_msgs::RepeatParams into a j2735_msgs::RepeatParams
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const cav_msgs::RepeatParams& in_msg, j2735_msgs::RepeatParams& out_msg);
+
+/**
+ * @brief Convert the contents of a cav_msgs::TrafficControlBounds into a j2735_msgs::TrafficControlBounds
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const cav_msgs::TrafficControlBounds& in_msg, j2735_msgs::TrafficControlBounds& out_msg);
+
+/**
+ * @brief Convert the contents of a cav_msgs::TrafficControlDetail into a j2735_msgs::TrafficControlDetail
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const cav_msgs::TrafficControlDetail& in_msg, j2735_msgs::TrafficControlDetail& out_msg);
+
+/**
+ * @brief Convert the contents of a cav_msgs::TrafficControlGeometry into a j2735_msgs::TrafficControlGeometry
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const cav_msgs::TrafficControlGeometry& in_msg, j2735_msgs::TrafficControlGeometry& out_msg);
+
+/**
+ * @brief Convert the contents of a cav_msgs::TrafficControlMessage into a j2735_msgs::TrafficControlMessage
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const cav_msgs::TrafficControlMessage& in_msg, j2735_msgs::TrafficControlMessage& out_msg);
+
+/**
+ * @brief Convert the contents of a cav_msgs::TrafficControlMessageV01 into a j2735_msgs::TrafficControlMessageV01
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const cav_msgs::TrafficControlMessageV01& in_msg, j2735_msgs::TrafficControlMessageV01& out_msg);
+
+/**
+ * @brief Convert the contents of a cav_msgs::TrafficControlParams into a j2735_msgs::TrafficControlParams
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const cav_msgs::TrafficControlParams& in_msg, j2735_msgs::TrafficControlParams& out_msg);
+
+/**
+ * @brief Convert the contents of a cav_msgs::TrafficControlSchedule into a j2735_msgs::TrafficControlSchedule
+ *
+ * @param in_msg The message to be converted
+ * @param out_msg The message to store the output
+ *
+ * Unit conversions and presence flags are handled
+ */
+void convert(const cav_msgs::TrafficControlSchedule& in_msg, j2735_msgs::TrafficControlSchedule& out_msg);
 }  // namespace geofence_control
 }  // namespace j2735_convertor
