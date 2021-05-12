7aa9cb2e16dd88b369367e0dd7f693a043d748cc
dan-du-car <dan.du@leidos.com>
2021-05-12 10:36:17 -0400

    update

--- carma-messenger-ui/website/widgets/eventManagement/widget.js
+++ carma-messenger-ui/website/widgets/eventManagement/widget.js
@@ -113,30 +113,30 @@ CarmaJS.WidgetFramework.eventManagement = (function () {
         var MinGapValue =  $('#MinGap').val();
         var AdvisorySpeedValue =  $('#AdvisorySpeed').val();
         if(UpTrackValue=="" || isNaN(UpTrackValue) 
-            || UpTrackValue >= CarmaJS.Config.getUpTrackRange().MAX 
-            || UpTrackValue <= CarmaJS.Config.getUpTrackRange().MIN)
+            || UpTrackValue > CarmaJS.Config.getUpTrackRange().MAX 
+            || UpTrackValue < CarmaJS.Config.getUpTrackRange().MIN)
         {
             alert("Up Track value is required, should be greater than or equal to "+CarmaJS.Config.getUpTrackRange().MIN+" and less than or equal to "+ CarmaJS.Config.getUpTrackRange().MAX);
             return false;
         }
 
         if(DownTrackValue=="" || isNaN(DownTrackValue) 
-        || DownTrackValue >= CarmaJS.Config.getDownTrackRange().MAX 
-        || DownTrackValue <= CarmaJS.Config.getDownTrackRange().MIN){
+        || DownTrackValue > CarmaJS.Config.getDownTrackRange().MAX 
+        || DownTrackValue < CarmaJS.Config.getDownTrackRange().MIN){
             alert("Down Track value  is required and should be greater than or equal to "+CarmaJS.Config.getDownTrackRange().MIN+" and less than  or equal to "+ CarmaJS.Config.getDownTrackRange().MAX);
             return false;
         }
 
         if(MinGapValue==""|| isNaN(MinGapValue) 
-        || MinGapValue >= CarmaJS.Config.getMinGapRange().MAX 
-        || MinGapValue <= CarmaJS.Config.getMinGapRange().MIN){
+        || MinGapValue > CarmaJS.Config.getMinGapRange().MAX 
+        || MinGapValue < CarmaJS.Config.getMinGapRange().MIN){
             alert("Minimum Gap value  is required and should be greater than or equal to "+CarmaJS.Config.getMinGapRange().MIN+" and less than  or equal to "+ CarmaJS.Config.getMinGapRange().MAX);
             return false;
         }
 
         if(AdvisorySpeedValue==""|| isNaN(AdvisorySpeedValue) 
-        || AdvisorySpeedValue >= CarmaJS.Config.getAdvisorySpeedRange().MAX 
-        || AdvisorySpeedValue <= CarmaJS.Config.getAdvisorySpeedRange().MIN){
+        || AdvisorySpeedValue > CarmaJS.Config.getAdvisorySpeedRange().MAX 
+        || AdvisorySpeedValue < CarmaJS.Config.getAdvisorySpeedRange().MIN){
             alert("Advisory Speed value  is required and should be greater than or equal to "+CarmaJS.Config.getAdvisorySpeedRange().MIN+" and less than  or equal to "+ CarmaJS.Config.getAdvisorySpeedRange().MAX);
             return false;
         }
