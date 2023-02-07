f8498ca7be8df96631659e41ace67c769c5d2dee
CARMA Developer <jonsmet37@gmail.com>
2023-02-07 12:44:49 -0800

    Update configuration parameter comments

--- carma-messenger-core/emergency_response_vehicle_plugin/include/emergency_response_vehicle_plugin/emergency_response_vehicle_plugin_config.hpp
+++ carma-messenger-core/emergency_response_vehicle_plugin/include/emergency_response_vehicle_plugin/emergency_response_vehicle_plugin_config.hpp
@@ -27,15 +27,17 @@ namespace emergency_response_vehicle_plugin
    */
   struct Config
   {
-    bool enable_emergency_response_vehicle_plugin = false;          // A flag indicating whether this plugin shall be activated. If activated, this plugin will publish
-                                                                    // BSMs when the vehicle's lights and sirens are active.
+    bool enable_emergency_response_vehicle_plugin = false;          //  A flag indicating whether this plugin shall be activated. If activated, this plugin will publish
+                                                                    //  the Emergency Response Vehicle's BSMs and process incoming UDP packets on the local port provided
+                                                                    //  in the 'listening_port' parameter.
 
     double bsm_generation_frequency = 10.0;                         // (Hz) The frequency at which BSMs will be generated and published by this plugin. 
 
     double min_distance_to_next_destination_point = 30.0;           // (Meters) The distance that the ERV must be from its next route destination point before the point is removed from
                                                                     // the list of future route destination points.
 
-    std::string emergency_route_file_path = "DEFAULT_PATH";         // The path on the host PC that contains the pre-defined points existing along active emergency vehicle's route.
+    std::string emergency_route_file_path = "DEFAULT_PATH";         // The path to the file on the host PC for the .csv file that contains the pre-defined points existing along active emergency vehicle's route.
+                                                                    // The folder containing this .csv file is expected to be /opt/carma/routes (Example: "/opt/carma/routes/example_route.csv").
 
     int listening_port = 1610;                                      // The listening port that this node’s UDP socket will bind to in order to receive data related to the status 
                                                                     // of the ERV’s emergency sirens and lights.
