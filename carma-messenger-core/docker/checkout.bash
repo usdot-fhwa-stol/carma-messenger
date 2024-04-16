288e5adfcb35cf336064fdc6be765ab66d8b0cfa
dev <paul.k.bourelly@leidos.com>
2024-04-16 15:38:36 -0400

    Updates

--- carma-messenger-core/docker/checkout.bash
+++ carma-messenger-core/docker/checkout.bash
@@ -34,6 +34,10 @@ while [[ $# -gt 0 ]]; do
             ;;
       esac
 done
-git clone https://github.com/usdot-fhwa-stol/carma-msgs.git ${dir}src/CARMAMsgs --branch $BRANCH
-git clone https://github.com/usdot-fhwa-stol/carma-utils.git ${dir}src/CARMAUtils --branch $BRANCH
+if [[ -n "$BRANCH" ]];then
+      git clone https://github.com/usdot-fhwa-stol/carma-msgs.git ${dir}src/CARMAMsgs --branch $BRANCH
+      git clone https://github.com/usdot-fhwa-stol/carma-utils.git ${dir}src/CARMAUtils --branch $BRANCH
+else
+      git clone https://github.com/usdot-fhwa-stol/carma-msgs.git ${dir}src/CARMAMsgs --branch develop
+      git clone https://github.com/usdot-fhwa-stol/carma-utils.git ${dir}src/CARMAUtils --branch develop
 
