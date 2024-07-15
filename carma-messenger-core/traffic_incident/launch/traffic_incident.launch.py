f8af491a15347cfee2497dc8bcb38e65f9902c99
John Chrosniak <chrosniakj@gmail.com>
2024-07-15 10:49:18 -0400

    added automatic lifecycle transitioning

--- carma-messenger-core/traffic_incident/launch/traffic_incident.launch.py
+++ carma-messenger-core/traffic_incident/launch/traffic_incident.launch.py
@@ -16,9 +16,10 @@ from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, TimerAction, RegisterEventHandler
+from launch.substitutions import LaunchConfiguration, FindExecutable
 from carma_ros2_utils.launch.get_current_namespace import GetCurrentNamespace
+from launch.event_handlers import OnExecutionComplete
 
 import os
 
@@ -34,6 +35,11 @@ def generate_launch_description():
     declare_log_level_arg = DeclareLaunchArgument(
         name="log_level", default_value="WARN"
     )
+
+    # Declare the configuration_delay launch argument
+    configuration_delay = LaunchConfiguration('configuration_delay')
+    declare_configuration_delay_arg = DeclareLaunchArgument(
+        name ='configuration_delay', default_value='4.0')
 
     traffic_incident_param_file = os.path.join(
         get_package_share_directory("traffic_incident"), "config/parameters.yaml"
@@ -65,9 +71,35 @@ def generate_launch_description():
         ],
     )
 
+    ros2_cmd = FindExecutable(name='ros2')
+
+    process_configure_traffic_incident = ExecuteProcess(
+        cmd=[ros2_cmd, "lifecycle", "set", "/traffic_incident", "configure"],
+    )
+
+    configuration_trigger = TimerAction(
+        period=configuration_delay,
+        actions=[
+            process_configure_traffic_incident,
+        ]
+    )
+
+    configured_event_handler_traffic_incident = RegisterEventHandler(OnExecutionComplete(
+            target_action=process_configure_traffic_incident,
+            on_completion=[ 
+                ExecuteProcess(
+                    cmd=[ros2_cmd, "lifecycle", "set", "/traffic_incident", "activate"],
+                )
+            ]
+        )
+    )
+
     return LaunchDescription(
         [
             declare_log_level_arg,
+            declare_configuration_delay_arg,
             container,
+            configuration_trigger,
+            configured_event_handler_traffic_incident
         ]
     )
