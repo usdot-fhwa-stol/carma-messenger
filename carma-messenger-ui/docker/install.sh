78e337a59b3c6aff0938cab3f034ab6309a39937
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2025-07-14 10:41:51 -0400

    Try php:8.2-apache-bullseye

--- carma-messenger-ui/docker/install.sh
+++ carma-messenger-ui/docker/install.sh
@@ -18,7 +18,7 @@ set -ex
 
 # Configure apt with Docker repos
 apt-get update
-apt-get -y install apt-transport-https \
+apt-get -y --force-yes install apt-transport-https \
         ca-certificates \
         curl \
         gnupg2 \
@@ -29,17 +29,17 @@ add-apt-repository \
         $(lsb_release -cs) \
         stable"
 
-# Ensure docker group id is 991. CARMA UI is dependent on having the group id be the same between the image and vehicle PC.
+# Ensure docker group id is 999. CARMA UI is dependent on having the group id be the same between the image and vehicle PC.
 if [[ -z $(grep  -i "docker" /etc/group) ]]; then
-    	echo "User docker does not exists in /etc/group, creating docker group id of 991"
-   	    addgroup --gid 991 docker
+    	echo "User docker does not exists in /etc/group, creating docker group id of 999"
+   	    addgroup --gid 999 docker
 else
 	echo "User docker already exists in /etc/group."
 
-	if [[ $(grep  -i "docker" /etc/group) == *"docker:x:991"* ]]; then
+	if [[ $(grep  -i "docker" /etc/group) == *"docker:x:999"* ]]; then
 	    echo "Docker group id is correct"
 	else
-	    echo "ERROR: CARMA requires the docker group id 991 in the host PC. Please update the Host PC to correct this before trying again."
+	    echo "ERROR: CARMA requires the docker group id 999 in the host PC. Please update the Host PC to correct this before trying again."
 	    exit
 	fi
 fi
